11f60ac7dc0e513703a72747621c66885ea1d2f2
Iandi Santulus <iandisantulusn@gmail.com>
2021-07-25 18:41:29 +0700

    added other constants

---
+++ lib/core/constants/app_error_messages.dart
@@ -0,0 +1,9 @@
+
+class AppErrorMessages {
+  static const String timeOutError = 'Connection time out. Please check your internet connection and try again.';
+  static const String socketError = 'No connection. Please connect to the internet and try again.';
+  static const String serverError = 'Server error. Please try again later.';
+  static const String unauthorizedError = 'Request Error: Unauthorized request.';
+  static const String unknownError = 'An unknown error occurred. Please try again.';
+  static const String unknownRequestError = 'An unknown error occurred while requesting to server. Please try again.';
+}
---
+++ lib/core/constants/app_routes.dart
@@ -0,0 +1,6 @@
+
+class AppRoutes {
+  static const String main = '\\';
+  static const String l2channel = '\\l2';
+  static const String l3channel = '\\l3';
+}
