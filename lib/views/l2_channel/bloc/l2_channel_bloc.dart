1f292eec369c7e9df60477e9a41c787a74139f01
Iandi Santulus <iandisantulusn@gmail.com>
2021-08-04 20:53:33 +0700

    removed Debounce Mechanism on L2Channel BLoC

--- lib/views/l2_channel/bloc/l2_channel_bloc.dart
+++ lib/views/l2_channel/bloc/l2_channel_bloc.dart
@@ -40,48 +40,48 @@ class L2ChannelBloc extends Bloc<L2ChannelEvent, L2ChannelState> {
     );
   }
 
-  bool _isNonDebounceStates(L2Transition state) {
-    return (state.nextState is L2ChannelUpdatedState &&
-            !(state.nextState as L2ChannelUpdatedState)
-                .updateResponse
-                .isUpdatedEvent) ||
-        state.nextState is! L2ChannelUpdatedState;
-  }
+  // bool _isNonDebounceStates(L2Transition state) {
+  //   return (state.nextState is L2ChannelUpdatedState &&
+  //           !(state.nextState as L2ChannelUpdatedState)
+  //               .updateResponse
+  //               .isUpdatedEvent) ||
+  //       state.nextState is! L2ChannelUpdatedState;
+  // }
 
-  bool _isDebounceStates(L2Transition state) => !_isNonDebounceStates(state);
-
-  @override
-  L2TransitionStream transformTransitions(L2TransitionStream transitions) {
-    final nonDebounceStates = transitions.where(_isNonDebounceStates);
-    final debounceStates = transitions.where(_isDebounceStates).debounceTime(
-      const Duration(milliseconds: 150),
-    );
-    return nonDebounceStates.mergeWith([debounceStates]);
-  }
+  // bool _isDebounceStates(L2Transition state) {
+  //   return !_isNonDebounceStates(state);
+  // }
 
   // @override
-  // L3TransitionStream transformEvents(
-  //   L3Event events,
-  //   L3TransitionFn transitionFn,
-  // ) {
+  // L2TransitionStream transformTransitions(L2TransitionStream transitions) {
+  //   final nonDebounceStates = transitions.where(_isNonDebounceStates);
 
+  //   final debounceStates = transitions.where(_isDebounceStates).debounceTime(const Duration(milliseconds: 150));
+
+  //   return nonDebounceStates.mergeWith([debounceStates]);
+  // }
+
+  // @override
+  // L2TransitionStream transformEvents(
+  //   L2Event events,
+  //   L2TransitionFn transitionFn,
+  // ) {
   //   final nonDebounceEvents = events.where(
-  //     (event) => event is! UpdatedL3ChannelEvent,
+  //     (event) => event is! UpdatedL2ChannelEvent,
   //   );
 
   //   final debouncedEvents = events
   //       .where(
-  //         (event) => event is UpdatedL3ChannelEvent,
+  //         (event) => event is UpdatedL2ChannelEvent,
   //       )
   //       .debounceTime(
-  //         const Duration(milliseconds: 250),
+  //         const Duration(milliseconds: 500),
   //       );
-
-  //   // return super.transformEvents(
-  //   //   nonDelayedEvents.mergeWith([delayedEvents]),
-  //   //   transitionFn,
-  //   // ); //.switchMap(transitionFn);
-  //   return nonDebounceEvents.mergeWith([debouncedEvents]).switchMap(transitionFn);
+  //   return super.transformEvents(
+  //     nonDebounceEvents.mergeWith([debouncedEvents]),
+  //     transitionFn,
+  //   ); //.switchMap(transitionFn);
+  //   // return nonDebounceEvents.mergeWith([debouncedEvents]).switchMap(transitionFn);
   // }
 
   @override
@@ -93,7 +93,7 @@ class L2ChannelBloc extends Bloc<L2ChannelEvent, L2ChannelState> {
     } else if (event is SubscribedL2ChannelEvent) {
       yield* _subscribedl2Channel(event.subscribedResponse);
     } else if (event is UnsubscribeL2ChannelEvent) {
-      _unsubscribel2Channel();
+      yield* _unsubscribel2Channel();
     } else if (event is UnsubscribedL2ChannelEvent) {
       yield* _unsubscribedl2Channel();
     } else if (event is UpdatedL2ChannelEvent) {
@@ -126,7 +126,8 @@ class L2ChannelBloc extends Bloc<L2ChannelEvent, L2ChannelState> {
     );
   }
 
-  void _unsubscribel2Channel() {
+  L2State _unsubscribel2Channel() async* {
+    yield L2ChannelUnsubscribingState(state.symbol);
     final l3ChannelUnsubscribeRequest = WSL2Request.unsubscribe(
       symbol: state.symbol,
     ).toJson();
@@ -140,19 +141,8 @@ class L2ChannelBloc extends Bloc<L2ChannelEvent, L2ChannelState> {
   L2State _snapshotl2Channel(
     WSL2Response snapshotResponse,
   ) async* {
-    // final totalBidOrder = snapshotResponse.bids.fold<int>(
-    //   0,
-    //   (prev, bid) => prev + bid.numberOfOrders,
-    // );
-    // final totalAskOrder = snapshotResponse.asks.fold<int>(
-    //   0,
-    //   (prev, ask) => prev + ask.numberOfOrders,
-    // );
-
     yield L2ChannelUpdatedState(
       symbol: state.symbol,
-      // totalAskOrder: totalAskOrder,
-      // totalBidOrder: totalBidOrder,
       updateResponse: snapshotResponse,
     );
   }
@@ -160,24 +150,17 @@ class L2ChannelBloc extends Bloc<L2ChannelEvent, L2ChannelState> {
   L2State _updatel2Channel(
     WSL2Response updateResponse,
   ) async* {
-    final currentState = state as L2ChannelUpdatedState;
+    if (state is L2ChannelUpdatedState) {
+      final currentState = state as L2ChannelUpdatedState;
 
-    final updatedResponse =
-        currentState.updateResponse.updatePrices(updateResponse);
-    // final totalBidOrder = updatedResponse.bids.fold<int>(
-    //   0,
-    //   (prev, bid) => prev + bid.numberOfOrders,
-    // );
-    // final totalAskOrder = updatedResponse.asks.fold<int>(
-    //   0,
-    //   (prev, ask) => prev + ask.numberOfOrders,
-    // );
-    yield L2ChannelUpdatedState(
-      symbol: currentState.symbol,
-      // totalAskOrder: totalAskOrder,
-      // totalBidOrder: totalBidOrder,
-      updateResponse: updatedResponse,
-    );
+      final updatedResponse =
+          currentState.updateResponse.updatePrices(updateResponse);
+
+      yield L2ChannelUpdatedState(
+        symbol: currentState.symbol,
+        updateResponse: updatedResponse,
+      );
+    }
   }
 
   L2State _rejectedl2Channel(
@@ -207,7 +190,9 @@ class L2ChannelBloc extends Bloc<L2ChannelEvent, L2ChannelState> {
       add(UnsubscribedL2ChannelEvent(wsResponse));
     } else if (wsResponse is WSL2Response &&
         wsResponse.symbol == state.symbol &&
-        wsResponse.channel == WSChannel.l2) {
+        wsResponse.channel == WSChannel.l2 &&
+        (state is! L2ChannelUnsubscribingState ||
+            state is! L2ChannelUnsubscribedState)) {
       add(UpdatedL2ChannelEvent(wsResponse));
     } else if (wsResponse is WSRejected && wsResponse.channel == WSChannel.l2) {
       add(RejectedL2ChannelEvent(wsResponse));
